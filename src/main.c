d569c66e2875d02a371deaa2e747f8db01b4126e
Matheus Campos <mcquintao.qui@gmail.com>
2024-05-07 02:21:55 -0300

    GLFW with nuklear AND GLAD!

--- src/main.c
+++ src/main.c
@@ -1,37 +1,115 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <limits.h>
+#include <time.h>
+
 #include "../include/glad/glad.h"
+// #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL3_IMPLEMENTATION
+#define NK_KEYSTATE_BASED_INPUT
+#include "../include/nuklear/nuklear.h"
+#include "../include/nuklear/nuklear_glfw_gl3.h"
 
-int main(void) {
-    printf("Hello World\n");
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
 
-    GLFWwindow* window;
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
 
-    if (!glfwInit()){
-        return -1;
+
+static void error_callback(int e, const char *d)
+{printf("Error %d: %s\n", e, d);}
+
+int main(void)
+{
+    /* Platform */
+    struct nk_glfw glfw = {0};
+    static GLFWwindow *win;
+    int width = 0, height = 0;
+    struct nk_context *ctx;
+    struct nk_colorf bg;
+
+    /* GLFW */
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stdout, "[GFLW] failed to init!\n");
+        exit(1);
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(640, 480, "Window!", NULL, NULL);
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-        printf("Não foi possivel carregar o OPENGL");
-        glfwTerminate();
-        return -1;
-    }
-
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Demo", NULL, NULL);
+    glfwMakeContextCurrent(win);
 
 
-    while (!glfwWindowShouldClose(window)) {
+    /* OpenGL */ 
+
+//For GLEW :( (Need to be installed on system)
+    // glewExperimental = 1;
+    // if (glewInit() != GLEW_OK) {
+    //     fprintf(stderr, "Failed to setup GLEW\n");
+    //     exit(1);
+    // }
+
+// ---
+
+//For GLAD :) (../include/glad/glad.h)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		printf("failed initializing glad");
+		return -1;
+	}
+// ---
+
+    ctx = nk_glfw3_init(&glfw, win, NK_GLFW3_INSTALL_CALLBACKS);
+
+    {struct nk_font_atlas *atlas;
+    nk_glfw3_font_stash_begin(&glfw, &atlas);
+
+    nk_glfw3_font_stash_end(&glfw);
+}
+
+    bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
+    while (!glfwWindowShouldClose(win))
+    {
+        glfwGetFramebufferSize(win, &width, &height);
+
+        glViewport(0, 0, width, height);
         glfwPollEvents();
+        nk_glfw3_new_frame(&glfw);
 
+        /* GUI */
+        if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
+            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+            NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+        {
+            nk_layout_row_static(ctx, 30, 80, 1);
+        }
+        nk_end(ctx);
+
+        
         glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(bg.r, bg.g, bg.b, bg.a);
 
-        glfwSwapBuffers(window);
+        nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+        glfwSwapBuffers(win);
     }
+    nk_glfw3_shutdown(&glfw);
     glfwTerminate();
-
     return 0;
 }
